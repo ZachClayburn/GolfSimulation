2000151a430f0274c7ffe4496382cd9b541b2aed
Zach Clayburn <zachclayburn@gmail.com>
2017-04-17 23:10:38 -0600

    Added plotting controls.

--- MainGUI.m
+++ MainGUI.m
@@ -22,7 +22,7 @@ function varargout = MainGUI(varargin)
 
 % Edit the above text to modify the response to help MainGUI
 
-% Last Modified by GUIDE v2.5 11-Apr-2017 20:55:51
+% Last Modified by GUIDE v2.5 17-Apr-2017 23:10:02
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -412,3 +412,42 @@ function windAngleText_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in multiPlot.
+function multiPlot_Callback(hObject, eventdata, handles)
+% hObject    handle to multiPlot (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of multiPlot
+
+
+% --- Executes on selection change in plotCountMenu.
+function plotCountMenu_Callback(hObject, eventdata, handles)
+% hObject    handle to plotCountMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns plotCountMenu contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from plotCountMenu
+
+
+% --- Executes during object creation, after setting all properties.
+function plotCountMenu_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to plotCountMenu (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in plotButton.
+function plotButton_Callback(hObject, eventdata, handles)
+% hObject    handle to plotButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
